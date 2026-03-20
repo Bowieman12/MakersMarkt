426dc3bc6ef59419995ad7fd4c02bd12b89570f9
Bowieman12 <Larsschipperen@gmail.com>
2026-03-20 13:59:37 +0100

    kleine error fix

--- MakersMarkt/Data/AppDbContext.cs
+++ MakersMarkt/Data/AppDbContext.cs
@@ -14,7 +14,7 @@ namespace MakersMarkt.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySql(
-                ConfigurationManager.ConnectionStrings["BarrocIntensDatabase"].ConnectionString,
+                ConfigurationManager.ConnectionStrings["MakersMarktDb"].ConnectionString,
                 ServerVersion.Parse("8.0.30") 
             );
         }
